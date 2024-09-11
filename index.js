2ea2dde69b0b14fecccdb0e90ef6859347df775e
tetehoma <tetehoma@gmail.com>
2024-09-11 22:09:31 +0900

    fundamentoseconstantes

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-console.log('2')
-const mensagem = 'Olaá mundo'
 
-console.log(mensagem)
+
+
+//const start é a função principal que chama o menu
+const start = () => {
+    while (true) {
+        let option = "sair" 
+            switch (option) {
+                case "cadastrar":
+                    console.log("vamos cadastrar")
+                    break
+                case "listar":
+                    console.log("vamos listar")
+                    break
+                case "sair":
+                    return //interrompe a função
+            }
+        //o return irá interromper a função inteira 
+    }
+}
+
+start()
